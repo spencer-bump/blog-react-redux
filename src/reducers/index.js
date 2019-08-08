5a7b6fd13a344cbfc4b0cb6744ca51771d86bb24
Spencer Eldred <eldredspencer@gmail.com>
2019-08-08 09:44:50 -1000

    new react-redux redux-thunk app

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+
+// ***************************
+// Initial Dummy Valid Reducer
+// ***************************
+export default combineReducers({
+  fakeReducer: () => "hey there"
+});
